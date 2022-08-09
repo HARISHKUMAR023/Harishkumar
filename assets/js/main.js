92553e8495fe628249c26293dfbcb67136e4174f
Harishkumar <harishkumarsp023@gmail.com>
2022-08-09 08:22:15 +0530

    update the youtube channel link

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let ytube = document.getElementById("youtube");
+ytube.addEventListener("click", you)
+function you() {
+    window.location.href = "https://www.youtube.com/channel/UCT0qh0BK9kGe2atm0ea2UZQ";
+
+}
