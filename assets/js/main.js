36828086d189d2f0466be0b392d97bb3819800d3
Harishkumar <harishkumarsp023@gmail.com>
2023-01-01 13:16:20 +0530

    mobile nav menu is added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 let ytube = document.getElementById("youtube");
+let navbtn = document.getElementById("nav");
+
+
 ytube.addEventListener("click", you)
 function you() {
     window.location.href = "https://www.youtube.com/channel/UCT0qh0BK9kGe2atm0ea2UZQ";
 
 }
+navbtn.addEventListener("click", () => {
+    let headermenu = document.querySelector('ul.header--menu');
+    const navicon = document.querySelectorAll(".navicon");
+    headermenu.classList.toggle('show')
+    navicon.forEach(icon => {
+        icon.classList.toggle("hidden");
+    })
+})
